b22f06e84693b6a9d46994bd94dea3300c8e2168
flysquare0 <uarda@teklifbilisim.com>
2022-05-12 22:02:09 +0300

    completed localstorage algorythm  without delete function

--- src/app/ToDo/todo.component.ts
+++ src/app/ToDo/todo.component.ts
@@ -10,27 +10,29 @@ import {HttpClient} from '@angular/common/http';
 
 export class TodoComponent {
   showAll : boolean = true;
-
+  items : any | null = JSON.parse(window.localStorage.getItem('todos') || '{}');
   addTodo(input: any) {
     if (input != '') {
       this.items.push({id:this.items.length+1,desc: input, action: false});
+      window.localStorage.setItem('todos',JSON.stringify(this.items));
     } else {
       alert("Please check all fields");
     }
   }
 
-  items = [{id:1,desc: "Çamaşırları Yıka", action: false}, {id:2,desc: "Evi Süpür", action: true}, {id:3,desc: "Ders Çalış", action: false}];
-
   getItems(){
     if (this.showAll){
       return this.items;
     }
-    return this.items.filter(item => item.action === false);
+    return this.items.filter((item : any) => item.action === false);
   }
+
   doneItem(itemId:any){
     this.items[itemId-1]['action'] = true;
+    window.localStorage.setItem('todos',JSON.stringify(this.items));
   }
+
   deleteItem(itemId:any){
-    this.items = this.items.filter(item => item.id != itemId)
+    //this.items = this.items.filter(item => item.id != itemId)
   }
 }
